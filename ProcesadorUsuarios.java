378b7f4b1ba8a846403d08a56d9c93c4331532b9
Nacho <nachorizquez@gmail.com>
2026-01-29 20:35:54 +0100

    Refactorización de ProcesadorUsuarios (Extract Method, Extract Constant)

--- ProcesadorUsuarios.java
+++ ProcesadorUsuarios.java
@@ -6,27 +6,39 @@ import java.util.List;
  */
 public class ProcesadorUsuarios {
 
+
+    private static final int ROL_INVITADO = 2;
+    private static final int ROL_ADMIN = 1;
+
     // Método con 'code smells': largo, números mágicos, malos nombres.
-    public String procesarLista(List<String> dataList) {
+    public String procesarLista(List<String> usuarios) {
         String admins = "";
         String invitados = "";
 
-        for (String u : dataList) {
+        for (String u : usuarios) {
             String[] parts = u.split(":"); // Formato "nombre:rol"
             if (parts.length == 2) {
-                String n = parts[0];
-                int r = Integer.parseInt(parts[1]);
+                String nombre = parts[0];
+                int rol = Integer.parseInt(parts[1]);
 
                 // Número Mágico: 1 es Admin
-                if (r == 1) {
-                    admins += n + ",";
+                if (rol == ROL_ADMIN) {
+                    admins = procesarAdmin(admins, nombre);
                 }
                 // Número Mágico: 2 es Invitado
-                else if (r == 2) {
-                    invitados += n + ",";
+                else if (rol == ROL_INVITADO) {
+                    invitados = procesarInvitado(invitados, nombre);
                 }
             }
         }
         return "Admins: " + admins + " | Invitados: " + invitados;
+    }
+
+    private String procesarAdmin(String admins, String nombre) {
+        return admins + nombre + ",";
+    }
+
+    private String procesarInvitado(String invitados, String nombre) {
+        return invitados + nombre + ",";
     }
 }
